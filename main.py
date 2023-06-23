37d05f580cc6aa74bb53aa91030489ad6c15c4b4
Krishan-Kant123 <108712328+Krishan-Kant123@users.noreply.github.com>
2023-06-23 21:20:28 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -6,20 +6,24 @@ import requests
 
 import json
 
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
 
-origins = ["*"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+]
+
+app = FastAPI(middleware=middleware)
 
 
 
